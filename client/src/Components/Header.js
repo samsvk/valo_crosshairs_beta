61e1091634cb9639319941d54caa5b3ef40d3698
samsvk <samsvk00@gmail.com>
2022-04-08 02:00:44 +0100

    updated styles & labelling

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,21 +1,33 @@
 import React from "react";
 import Search from "./Search";
+import { AiOutlineGithub } from "react-icons/ai";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default React.memo(() => {
   return (
-    <header className="header">
-      <div>
-        <h1>Crosshairs.gg</h1>
-        <p>
-          Is a lightweight crosshair index/directory that aims to publish
-          professionals crosshairs to the public. Perform better, with the best
-          crosshairs. Browse and find crosshairs that fits your preference and
-          see what top professional players are using.
-        </p>
+    <>
+      <div className="info">
+        <div className="info__label">What's new?</div>
+        <div className="info__github">
+          <AiOutlineGithub size={19} style={{ alignItems: "center" }} />
+          <span>Just shipped beta 0.0.1</span>
+          <MdKeyboardArrowRight size={20} style={{ alignItems: "center" }} />
+        </div>
       </div>
-      <div className="header__bottom">
-        <Search />
-      </div>
-    </header>
+      <header className="header">
+        <div>
+          <h1>Crosshairs.gg</h1>
+          <p>
+            Is a lightweight crosshair index/directory that aims to publish
+            professionals crosshairs to the public. Perform better, with the
+            best crosshairs. Browse and find crosshairs that fits your
+            preference and see what top professional players are using.
+          </p>
+        </div>
+        <div className="header__bottom">
+          <Search />
+        </div>
+      </header>
+    </>
   );
 });
